4f968fca65560685adaae30dd944a27b5e645bed
Andreas Triantafyllopoulos <and.triantafy@gmail.com>
2024-11-06 16:45:35 +0100

    Added safe call to F1 MLC scores

--- autrainer/metrics/multi_label_classification.py
+++ autrainer/metrics/multi_label_classification.py
@@ -3,6 +3,36 @@ import sklearn.metrics
 from .abstract_metric import BaseAscendingMetric
 
 
+def _safe_f1_score(
+    y_true,
+    y_pred,
+    *,
+    labels=None,
+    pos_label=1,
+    average="binary",
+    sample_weight=None,
+    zero_division="warn"
+):
+    multilabel = True
+    if (
+        (len(y_true.shape) == 1)
+        or (y_true.shape[0] == 1)
+        or (y_true.shape[1] == 1)
+    ):
+        multilabel = False
+    if average != "binary" and not multilabel:
+        average = "binary"
+    return sklearn.metrics.f1_score(
+        y_true=y_true,
+        y_pred=y_pred,
+        labels=labels,
+        pos_label=pos_label,
+        average=average,
+        sample_weight=sample_weight,
+        zero_division=zero_division
+    )
+
+
 class MLAccuracy(BaseAscendingMetric):
     def __init__(self):
         """Accuracy metric using `sklearn.metrics.accuracy_score`."""
@@ -14,7 +44,7 @@ class MLF1Macro(BaseAscendingMetric):
         """F1 macro metric using `sklearn.metrics.f1_score`."""
         super().__init__(
             name="ml-f1-macro",
-            fn=sklearn.metrics.f1_score,
+            fn=_safe_f1_score,
             average="macro",
         )
 
@@ -24,7 +54,7 @@ class MLF1Micro(BaseAscendingMetric):
         """F1 micro metric using `sklearn.metrics.f1_score`."""
         super().__init__(
             name="ml-f1-micro",
-            fn=sklearn.metrics.f1_score,
+            fn=_safe_f1_score,
             average="micro",
         )
 
@@ -34,6 +64,6 @@ class MLF1Weighted(BaseAscendingMetric):
         """F1 weighted metric using `sklearn.metrics.f1_score`."""
         super().__init__(
             name="ml-f1-weighted",
-            fn=sklearn.metrics.f1_score,
+            fn=_safe_f1_score,
             average="weighted",
         )
