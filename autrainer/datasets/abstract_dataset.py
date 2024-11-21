ef68a44c56ea055c93ed5548e68c063b50279a1f
Andreas Triantafyllopoulos <and.triantafy@gmail.com>
2024-11-21 19:39:51 +0100

    Fix transform collate_fn

--- autrainer/datasets/abstract_dataset.py
+++ autrainer/datasets/abstract_dataset.py
@@ -275,7 +275,9 @@ class AbstractDataset(ABC):
             batch_size=self.batch_size,
             shuffle=True,
             generator=self._generator,
-            collate_fn=default_data_collator,  # self.train_transform.get_collate_fn(self),
+            collate_fn=self.train_transform.get_collate_fn(
+                self, default=default_data_collator
+            ),
         )
 
     @cached_property
@@ -290,7 +292,9 @@ class AbstractDataset(ABC):
             batch_size=self.inference_batch_size,
             shuffle=False,
             generator=self._generator,
-            collate_fn=default_data_collator,  # self.dev_transform.get_collate_fn(self),
+            collate_fn=self.dev_transform.get_collate_fn(
+                self, default=default_data_collator
+            ),
         )
 
     @cached_property
@@ -305,7 +309,9 @@ class AbstractDataset(ABC):
             batch_size=self.inference_batch_size,
             shuffle=False,
             generator=self._generator,
-            collate_fn=default_data_collator,  # self.dev_transform.get_collate_fn(self),
+            collate_fn=self.test_transform.get_collate_fn(
+                self, default=default_data_collator
+            ),
         )
 
     def get_evaluation_data(
