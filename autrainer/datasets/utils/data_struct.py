179b1bd0f8d55b96abb0c19575648c96837cbd9d
Andreas Triantafyllopoulos <and.triantafy@gmail.com>
2024-11-26 19:00:20 +0100

    Rename label attribute to target

--- autrainer/datasets/utils/data_struct.py
+++ autrainer/datasets/utils/data_struct.py
@@ -7,10 +7,10 @@ import torch
 @dataclass
 class Data:
     features: torch.Tensor
-    label: Union[int, float, List[int], List[float]]
+    target: Union[int, float, List[int], List[float]]
     index: int
 
     def to(self, device):
         self.features.to(device)
-        self.label.to(device)
+        self.target.to(device)
         self.index.to(device)
