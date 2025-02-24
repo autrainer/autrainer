e457f7c06ae3978dcae465a425cc57ab8d43ffed
Philipp Wagner <philippwagner99@web.de>
2025-02-24 18:58:46 +0100

    Update wrapper class

--- autrainer/datasets/utils/dataset_wrapper.py
+++ autrainer/datasets/utils/dataset_wrapper.py
@@ -96,56 +96,63 @@ class DatasetWrapper(torch.utils.data.Dataset):
 class SegmentedDatasetWrapper(DatasetWrapper):
     def __init__(
         self,
-        window_size: float = 0.25,
-        hop_size: float = 0.25,
-        min_event_length: float = 0.25,
-        min_event_gap: float = 0.15,
-        **kwargs,
-    ):
-        """Wrapper around torch.utils.data.Dataset for segmented audio files.
-        If data is already windowed (has 'start' and 'end' columns), windowing parameters are ignored.
+        path: str,
+        features_subdir: str,
+        index_column: str,
+        target_column: Union[str, List[str]],
+        start_column: str,
+        end_column: str,
+        file_handler: AbstractFileHandler,
+        df: pd.DataFrame,
+        file_type: Optional[str] = None,
+        transform: Optional[SmartCompose] = None,
+        target_transform: Optional[AbstractTargetTransform] = None,
+    ) -> None:
+        """Wrapper for segmented audio files.
 
         Args:
-            window_size: Size of the segmented window in seconds. Ignored if data is pre-windowed.
-            hop_size: Hop size in seconds. Ignored if data is pre-windowed.
-            min_event_length: Minimum duration of an event in seconds. Ignored if data is pre-windowed.
-            min_event_gap: Minimum duration between events in seconds. Ignored if data is pre-windowed.
+            path: Root path to the dataset.
+            features_subdir: Subdirectory containing the features.
+            index_column: Index column of the dataframe.
+            target_column: Target column of the dataframe.
+            start_column: Name of column containing segment start times.
+            end_column: Name of column containing segment end times.
+            file_handler: File handler to load the data.
+            df: Dataframe containing the index and target column(s).
+            file_type: File type of the features. If `None`,
+                will not enforce a file_type. This can be useful
+                in case the dataset contains audio files
+                with different formats. Defaults to `None`.
+            transform: Transform to apply to the features. Defaults to None.
+            target_transform: Target transform to apply to the target.
+                Defaults to None.
         """
-        super().__init__(**kwargs)
-        self.is_prewindowed = (
-            "start" in self.df.columns and "end" in self.df.columns
+        self.start_column = start_column
+        self.end_column = end_column
+        super().__init__(
+            path=path,
+            features_subdir=features_subdir,
+            index_column=index_column,
+            target_column=target_column,
+            file_handler=file_handler,
+            df=df,
+            file_type=file_type,
+            transform=transform,
+            target_transform=target_transform,
         )
-        if not self.is_prewindowed:
-            if window_size is None:
-                raise ValueError(
-                    "window_size must be provided for non-windowed data"
-                )
-            self.window_size = window_size
-            self.hop_size = hop_size
-            self.min_event_length = min_event_length
-            self.min_event_gap = min_event_gap
 
     def __getitem__(
         self,
         item: int,
     ) -> Tuple[torch.Tensor, Union[int, torch.Tensor], int]:
-        """Get item from the dataset.
-
-        Args:
-            item: Index of the item.
-
-        Returns:
-            Tuple containing the data, target and item index.
-        """
-
         index = self.df.index[item]
         item_path = self.df.loc[index, self.index_column]
         if isinstance(self.file_handler, AudioFileHandler):
             data = audiofile.read(
                 item_path,
-                offset=self.df.loc[index, "start"],
-                duration=self.df.loc[index, "end"]
-                - self.df.loc[index, "start"],
+                offset=self.df.loc[index, self.start_column],
+                duration=self.df.loc[index, self.end_column]
+                - self.df.loc[index, self.start_column],
                 always_2d=True,
             )[0]
             data = torch.from_numpy(data)
