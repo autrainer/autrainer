f8d8b17dd6c8b4511673b2fe80ef6d17b3de5a57
Andreas Triantafyllopoulos <and.triantafy@gmail.com>
2025-03-25 12:54:58 +0100

    Updated msp

--- autrainer/datasets/msppodcast.py
+++ autrainer/datasets/msppodcast.py
@@ -1,20 +1,20 @@
-import os
 from functools import cached_property
-from typing import Dict, List, Optional, Tuple, Union
+import os
+from typing import Dict, List, Optional, Union
 
 from omegaconf import DictConfig
 import pandas as pd
-import torch
 
 from autrainer.transforms import SmartCompose
 
 from .abstract_dataset import AbstractDataset
-from .aibo import Standardizer
 from .utils import (
     AbstractTargetTransform,
     LabelEncoder,
     MinMaxScaler,
+    MultiTargetMinMaxScaler,
 )
+
 
 class MSPPodcast(AbstractDataset):
     def __init__(
@@ -26,7 +26,7 @@ class MSPPodcast(AbstractDataset):
         tracking_metric: str,
         index_column: str,
         target_column: str,
-        file_type:str,
+        file_type: str,
         file_handler: Union[str, DictConfig, Dict],
         batch_size: int,
         inference_batch_size: Optional[int] = None,
@@ -34,22 +34,21 @@ class MSPPodcast(AbstractDataset):
         dev_transform: Optional[SmartCompose] = None,
         test_transform: Optional[SmartCompose] = None,
         stratify: Optional[List[str]] = None,
-        standardize: bool = False,
         categories: List[str] = None,
     ) -> None:
         """MSP-Podcast dataset.
 
         .. warning::
             There are multiple versions available for this dataset.
-            We recommend always using the latest one 
+            We recommend always using the latest one
             (v1.11 at the time of writing)
             but our code is set up to work with all versions
             (at least up to v1.11).
-        
+
         .. note::
             Note that after v1.7, the dataset features two test sets.
             We only use ``Test1``, as ``Test2`` was found to be biased
-            with respect to gender. 
+            with respect to gender.
             See https://doi.org/10.21437/Interspeech.2019-1708.
 
         .. note::
@@ -59,7 +58,7 @@ class MSPPodcast(AbstractDataset):
             ``EmoClass`` corresponds to categorical emotion classification,
             whereas ``EmoAct``, ``EmoVal``, and ``EmoDom`` to dimensional
             emotion regression for activation (arousal), valence, and dominance,
-            respectively.            
+            respectively.
 
         Args:
             path: Root path to the dataset.
@@ -81,18 +80,14 @@ class MSPPodcast(AbstractDataset):
             test_transform: Transform to apply to the test set.
                 Defaults to None.
             stratify: Columns to stratify the dataset on. Defaults to None.
-            standardize: Whether to standardize the data. Defaults to False.
-            categories: used to filter out specific emotional categories. 
+            categories: used to filter out specific emotional categories.
                 Useful for training on subset of data/classes, such as the classic
-                ["A", "H", "N", "S"] 4-class problem found in literature. 
+                ["A", "H", "N", "S"] 4-class problem found in literature.
                 Defaults to None.
         """
-        # TODO: expand for multitask regression
-        assert target_column in ("EmoClass", "EmoAct", "EmoVal", "EmoDom"), (
-            f"{target_column} not included in target list, please choose one of: "
-            "[EmoClass, EmoAct, EmoVal, EmoDom]"
+        task = (
+            "classification" if target_column == "EmoClass" else "regression"
         )
-        task = "classification" if target_column == "EmoClass" else "regression"
         self.categories = categories
         super().__init__(
             task=task,
@@ -112,21 +107,14 @@ class MSPPodcast(AbstractDataset):
             test_transform=test_transform,
             stratify=stratify,
         )
-        self.standardize = standardize
-        if self.standardize:
-            train_data = torch.cat([x for x, *_ in self.train_dataset])
-            standardizer = Standardizer(
-                mean=train_data.mean(0).tolist(),
-                std=train_data.std(0).tolist(),
-            )
 
-            self.train_transform += standardizer
-            self.dev_transform += standardizer
-            self.test_transform += standardizer
+    @property
+    def audio_subdir(self) -> str:
+        """Subfolder containing audio data.
 
-            self.train_dataset = self._init_dataset(
-                self.df_train, self.train_transform
-            )
+        Defaults to `Audios` for MSP-Podcast.
+        """
+        return "Audios"
 
     @staticmethod
     def download(path: str) -> None:  # pragma: no cover
@@ -134,7 +122,7 @@ class MSPPodcast(AbstractDataset):
         Download the MSP-Podcast dataset.
 
         As this dataset is not publicly-available, please download it manually
-        by contacting Prof. Carlos Busso: 
+        by contacting Prof. Carlos Busso:
         https://ecs.utdallas.edu/research/researchlabs/msp-lab/MSP-Podcast.html
 
         This function will not do anything.
@@ -143,10 +131,11 @@ class MSPPodcast(AbstractDataset):
         https://doi.org/10.1109/TAFFC.2017.2736999
         """
         return None
-    
-    def load_dataframes(
+
+    @cached_property
+    def _load_df(
         self,
-    ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    ) -> pd.DataFrame:
         """Load the dataframes.
 
         Overrides base class so there is no need
@@ -155,15 +144,28 @@ class MSPPodcast(AbstractDataset):
         Returns:
             Dataframes for training, development, and testing.
         """
-        df = pd.read_csv(os.path.join(self.path, "Labels", "labels_consensus.csv"))
+        df = pd.read_csv(
+            os.path.join(self.path, "Labels", "labels_consensus.csv")
+        )
         if self.categories is not None:
             df = df.loc[df["EmoClass"].isin(self.categories)]
+        return df.reset_index(drop=True)
 
-        df_train = df.loc[df["Split_Set"] == "Train"]
-        df_dev = df.loc[df["Split_Set"] == "Development"]
-        df_test = df.loc[df["Split_Set"] == "Test1"]
-        return df_train, df_dev, df_test
-    
+    @cached_property
+    def df_train(self):
+        df = self._load_df
+        return df.loc[df["Split_Set"] == "Train"].reset_index(drop=True)
+
+    @cached_property
+    def df_dev(self):
+        df = self._load_df
+        return df.loc[df["Split_Set"] == "Development"].reset_index(drop=True)
+
+    @cached_property
+    def df_test(self):
+        df = self._load_df
+        return df.loc[df["Split_Set"] == "Test1"].reset_index(drop=True)
+
     @cached_property
     def target_transform(self) -> AbstractTargetTransform:
         """Get the target transform.
@@ -178,12 +180,21 @@ class MSPPodcast(AbstractDataset):
                 self.df_train[self.target_column].unique().tolist()
             )
         elif self.task == "regression":
-            return MinMaxScaler(
-                minimum=self.df_train[self.target_column].min(),
-                maximum=self.df_train[self.target_column].max(),
-            )
+            if isinstance(self.target_column, list):
+                return MultiTargetMinMaxScaler(
+                    target=self.target_column,
+                    minimum=self.df_train[self.target_column].min().to_list(),
+                    maximum=self.df_train[self.target_column].max().to_list(),
+                )
+            else:
+                return MinMaxScaler(
+                    minimum=self.df_train[self.target_column].min(),
+                    maximum=self.df_train[self.target_column].max(),
+                )
         else:
-            raise NotImplementedError(f"{self.task} not supported for MSPPodcast")
+            raise NotImplementedError(
+                f"{self.task} not supported for MSPPodcast"
+            )
 
     @cached_property
     def output_dim(self) -> int:
@@ -197,6 +208,11 @@ class MSPPodcast(AbstractDataset):
         if self.task == "classification":
             return len(self.df_train[self.target_column].unique())
         elif self.task == "regression":
-            return 1
+            if isinstance(self.target_column, list):
+                return len(self.target_column)
+            else:
+                return 1
         else:
-            raise NotImplementedError(f"{self.task} not supported for MSPPodcast")
+            raise NotImplementedError(
+                f"{self.task} not supported for MSPPodcast"
+            )
