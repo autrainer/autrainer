cdbf47e265598d922dcd5c5bdd1f7fb2fe48723d
Simon Rampp <simon@rampp.dev>
2025-08-05 18:46:16 +0200

    handle hardcoded full path internal links

--- docs/source/_static/external_links.js
+++ docs/source/_static/external_links.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  $("a.external").attr("target", "_blank").attr("rel", "noopener noreferrer");
+$(function () {
+  $("a.external").each(function () {
+    const u = new URL(this.href, location.origin);
+    if (
+      u.hostname !== "autrainer.github.io" ||
+      !u.pathname.startsWith("/autrainer")
+    )
+      $(this).attr({ target: "_blank", rel: "noopener noreferrer" });
+  });
 });
