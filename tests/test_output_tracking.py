4d94fb04b15e5fd4f1136d9d6bd0cb579b6432e4
Andreas Triantafyllopoulos <and.triantafy@gmail.com>
2025-12-04 10:26:46 +0100

    Fixed tests

--- tests/test_output_tracking.py
+++ tests/test_output_tracking.py
@@ -5,7 +5,7 @@ import numpy.testing
 import pytest
 import torch
 import torch.testing
-from utils import BaseIndividualTempDir
+from .utils import BaseIndividualTempDir
 
 from autrainer.core.utils import Bookkeeping
 from autrainer.datasets import ToyDataset
@@ -36,57 +36,25 @@ class TestOutputsTracker(BaseIndividualTempDir):
         print(tracker)
 
     @pytest.mark.parametrize(
-        "dims",
-        [(1, 10), (2, 10), (2, 2, 10)],
+        "dims,tracker_class",
+        [
+            ((1, 10), OutputsTracker),
+            ((2, 10), OutputsTracker),
+            ((3, 2, 10), SequentialOutputsTracker),
+        ],
     )
-    def test_update(self, dims: List[int]) -> None:
-        tracker = OutputsTracker(
+    def test_update(self, dims: List[int], tracker_class) -> None:
+        tracker = tracker_class(
             export=True,
             prefix="foo",
             data=TOY_DATASET,
-            bookkeeping=Bookkeeping(self.temp_dir),
+            bookkeeping=Bookkeeping(self.temp_dir)
         )
         x = torch.rand(dims)
-        tracker.update(x, x, torch.Tensor([0.1]), torch.IntTensor([1]))
-        tracker.update(x, x, torch.Tensor([0.1]), torch.IntTensor([1]))
-        tracker.save("foo")
+        tracker.update(x, x, torch.Tensor([0.1]), torch.randint(high=10, size=(dims[0], 1)).squeeze(1))
+        tracker.update(x, x, torch.Tensor([0.1]), torch.randint(high=10, size=(dims[0], 1)).squeeze(1))
+        tracker.save("foo", reset=False)
 
         np.testing.assert_array_almost_equal(
-            torch.cat((x, x)).numpy(), tracker._outputs, 5
+            torch.cat((x, x)).numpy(), tracker._outputs
         )
-
-
-if __name__ == "__main__":
-    TOY_DATASET = ToyDataset(
-        task="classification",
-        size=100,
-        num_targets=10,
-        feature_shape=10,
-        dev_split=0.3,
-        test_split=0.3,
-        seed=1,
-        metrics=["autrainer.metrics.Accuracy"],
-        tracking_metric="autrainer.metrics.Accuracy",
-    )
-    tracker = SequentialOutputsTracker(
-        export=True,
-        prefix="foo",
-        data=TOY_DATASET,
-        bookkeeping=Bookkeeping("foo"),
-    )
-    x = torch.rand((3, 2, 10))
-    y = torch.rand((5, 4, 10))
-    tracker.update(
-        x,
-        x,
-        torch.Tensor([0.1]),
-        torch.IntTensor([0, 1, 2]),
-        mask=torch.Tensor([[1, 1], [1, 1], [1, 0]]),
-    )
-    tracker.update(y, y, torch.Tensor([0.1]), torch.IntTensor([3, 4, 5, 6, 7]))
-    tracker.save("bar", reset=False)
-
-    np.testing.assert_array_almost_equal(
-        torch.cat((torch.cat((x, torch.ones(3, 2, 10) * -100), dim=1), y)).numpy(),
-        tracker._outputs,
-    )
