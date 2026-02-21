7172e8e0724cc217a23b8fbe92f019f98fe796f4
Simon Rampp <simon@rampp.dev>
2026-02-21 21:15:21 +0100

    remove old callback manager

--- autrainer/training/__init__.py
+++ autrainer/training/__init__.py
@@ -1,11 +1,5 @@
-from .callback_manager import CallbackManager, CallbackSignature
 from .outputs_tracker import OutputsTracker
 from .training import ModularTaskTrainer
 
 
-__all__ = [
-    "CallbackManager",
-    "CallbackSignature",
-    "OutputsTracker",
-    "ModularTaskTrainer",
-]
+__all__ = ["ModularTaskTrainer", "OutputsTracker"]
